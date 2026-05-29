90ae50c1dd2470b5f4f76721658a9c37fd739141
Giulia Passoni <giulia.passoni@medicore.nl>
2026-05-29 08:18:19 +0200

    feat(FE): add hooks.

--- frontend/src/hooks/useCell.ts
+++ frontend/src/hooks/useCell.ts
@@ -1,7 +1,10 @@
 import {useQuery} from "@tanstack/react-query";
-import {getCellMetrics, getCells, getCellsStatus} from "../api/queries.ts";
+import {getCellEvents, getCellMetrics, getCells, getCellsStatus, getStatusSummary} from "../api/queries.ts";
+import type {CellMetrics, PersistedEvent, TimeWindow} from "../api/types.ts";
+import {mapSummaryToDomain} from "../utils/api.ts";
+import {transformEventData, transformPaginatedEvents, transformRawMetricsData} from "../utils/dataFormatting.ts";
 
-export function useCells(){
+export function useCells() {
     return useQuery({
         queryKey: ["cells"],
         queryFn: () => getCells(),
@@ -17,11 +20,30 @@ export function useCellsStatus() {
     });
 }
 
-export function useCellMetrics(cellId: string, window: string) {
+export function useStatusSummary() {
+    return useQuery({
+        queryKey: ["status", "summary"],
+        queryFn: () => getStatusSummary(),
+        refetchInterval: 30_000,
+        select: (data) => mapSummaryToDomain(data),
+    })
+}
+
+export function useCellMetrics(cellId: string, window: TimeWindow) {
     return useQuery({
         queryKey: ["metrics", cellId, window],
         queryFn: () => getCellMetrics(cellId, window),
         refetchInterval: 30_000,
+        select: (data: CellMetrics) => transformRawMetricsData(data, window),
+    })
+}
+
+export function useCellEvents(cellId: string, limit: number, offset: number) {
+    return useQuery({
+        queryKey: ["cell", cellId, "events", limit, offset],
+        queryFn: () => getCellEvents(cellId, limit, offset),
+        refetchInterval: 30_000,
+        select: (data) => transformPaginatedEvents(data), // fixme types and comments
     })
 }
 
