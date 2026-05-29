f5ec0e319d78fe69b52ddc7016271eb41fdb6c75
Giulia Passoni <giulia.passoni@medicore.nl>
2026-05-29 08:13:58 +0200

    feat(FE): update App.tsx with routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,20 @@
-import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+import {Layout} from "./components/organisms/Layout.tsx";
+import {FleetPage} from "./pages/FleetPage/FleetPage.tsx";
+import {CellPage} from "./pages/CellPage/CellPage.tsx";
+import {ErrorPage} from "./pages/ErrorPage/ErrorPage.tsx";
 
-import './App.css'
-import {useCells, useCellsStatus} from "./hooks/useCell.ts";
-
-function App() {
-  const [count, setCount] = useState(0)
-    const { data: cells, isLoading: isLoadingCells } = useCells()
-    const { data, isLoading } = useCellsStatus()
-
-    if(isLoading || isLoadingCells){
-        return <div>Loading...</div>
-    }
-
-  return (
-      <section id="center">
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-          {data && data.map((cell) => (
-              <div key={cell.cellId} className={`cell-card ${cell.state.toLowerCase()}`}>
-                  <h2>{cell.cellId}</h2>
-                  <p>State: <strong>{cell.state}</strong></p>
-                  <p>Since: {new Date(cell.since).toLocaleTimeString()}</p>
-              </div>
-          ))}
-          {cells && cells.map((cellId) => (
-              <div key={cellId}>
-                  <h2>{cellId}</h2>
-              </div>
-          ))}
-      </section>
-  )
-}
-
-export default App
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<FleetPage/>}/>
+                    <Route path="/cell/:cellId" element={<CellPage/>}/>
+                    <Route path="*" element={<ErrorPage/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+};
